eee4f5ff58bd206ad9bc7c5b66dddcbe932a5e45
James Baker <jlbaker361@gmail.com>
2024-05-08 19:58:16 -0400

    face stuff

--- src/experiment_helpers/measuring.py
+++ src/experiment_helpers/measuring.py
@@ -12,6 +12,8 @@ from transformers import Blip2Processor, Blip2ForConditionalGeneration
 from PIL import Image
 from sentence_transformers import SentenceTransformer
 from sentence_transformers.util import cos_sim as cos_sim_st
+from facenet_pytorch import MTCNN
+from .elastic_face_iresnet import get_face_embedding,get_iresnet_model
 
 def cos_sim(vector_i,vector_j)->float:
     return np.dot(vector_i,vector_j)/(norm(vector_i)*norm(vector_j))
@@ -40,7 +42,8 @@ def get_vit_embeddings(vit_processor: ViTImageProcessor, vit_model: BetterViTMod
     return vit_embedding_list,vit_style_embedding_list, vit_content_embedding_list
 
 @torch.no_grad()
-def get_metric_dict(evaluation_prompt_list:list, evaluation_image_list:list,image_list:list,accelerator:Accelerator=None):
+def get_metric_dict(evaluation_prompt_list:list, evaluation_image_list:list,src_image_list:list,accelerator:Accelerator=None,
+                    use_face:bool=False)->dict:
     metric_dict={}
     
     clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").eval()
@@ -49,12 +52,12 @@ def get_metric_dict(evaluation_prompt_list:list, evaluation_image_list:list,imag
         clip_model.to(accelerator.device)
         clip_model=accelerator.prepare(clip_model)
     
-    clip_inputs=clip_processor(text=evaluation_prompt_list, images=evaluation_image_list+image_list, return_tensors="pt", padding=True)
+    clip_inputs=clip_processor(text=evaluation_prompt_list, images=evaluation_image_list+src_image_list, return_tensors="pt", padding=True)
     clip_inputs["input_ids"]=clip_inputs["input_ids"].to(clip_model.device)
     clip_inputs["pixel_values"]=clip_inputs["pixel_values"].to(clip_model.device)
 
     clip_outputs = clip_model(**clip_inputs)
-    src_image_n=len(image_list)
+    src_image_n=len(src_image_list)
     text_embed_list=clip_outputs.text_embeds.cpu().detach().numpy()
     image_embed_list=clip_outputs.image_embeds.detach().cpu().numpy()[:-src_image_n]
     src_image_embed_list=clip_outputs.image_embeds.detach().cpu().numpy()[-src_image_n:]
@@ -86,7 +89,7 @@ def get_metric_dict(evaluation_prompt_list:list, evaluation_image_list:list,imag
         blip_conditional_gen=accelerator.prepare(blip_conditional_gen)
     
 
-    src_blip_caption_list=[get_caption(src_image,blip_processor,blip_conditional_gen) for src_image in image_list]
+    src_blip_caption_list=[get_caption(src_image,blip_processor,blip_conditional_gen) for src_image in src_image_list]
     image_blip_caption_list=[get_caption(image,blip_processor,blip_conditional_gen) for image in evaluation_image_list]
     embedding_model = SentenceTransformer('Alibaba-NLP/gte-large-en-v1.5', trust_remote_code=True).eval()
     if accelerator is not None:
@@ -111,7 +114,7 @@ def get_metric_dict(evaluation_prompt_list:list, evaluation_image_list:list,imag
         vit_model.to(accelerator.device)
         vit_model=accelerator.prepare(vit_model)
 
-    image_vit_embedding_list,image_vit_style_embedding_list,image_vit_content_embedding_list=get_vit_embeddings(vit_processor,vit_model,image_list)
+    image_vit_embedding_list,image_vit_style_embedding_list,image_vit_content_embedding_list=get_vit_embeddings(vit_processor,vit_model,src_image_list)
     evaluation_image_vit_embedding_list,evaluation_image_vit_style_embedding_list,evaluation_image_vit_content_embedding_list=get_vit_embeddings(vit_processor,vit_model,evaluation_image_list)
 
     vit_consistency_list=[]
@@ -125,7 +128,7 @@ def get_metric_dict(evaluation_prompt_list:list, evaluation_image_list:list,imag
         evaluation_vit_embedding=evaluation_image_vit_embedding_list[i]
         evaluation_vit_content_embedding=evaluation_image_vit_content_embedding_list[i]
         evaluation_vit_style_embedding=evaluation_image_vit_style_embedding_list[i]
-        for k in range(len(image_list)):
+        for k in range(len(src_image_list)):
             image_vit_embedding=image_vit_embedding_list[k]
             image_vit_style_embedding=image_vit_style_embedding_list[k]
             image_vit_content_embedding=image_vit_content_embedding_list[k]
@@ -157,6 +160,25 @@ def get_metric_dict(evaluation_prompt_list:list, evaluation_image_list:list,imag
     metric_dict[AESTHETIC_SCORE]=np.mean(
         [aesthetic_scorer(evaluation_image).cpu().numpy()[0] for evaluation_image in evaluation_image_list]
     )
+
+    if use_face:
+        mtcnn=MTCNN(device=accelerator.device)
+        mtcnn.requires_grad_(True)
+        mtcnn.eval()
+        iresnet=get_iresnet_model(accelerator.device)
+        mtcnn,iresnet=accelerator.prepare(mtcnn,iresnet)
+        image_face_embedding_list=get_face_embedding(evaluation_image_list,mtcnn,iresnet,10)
+        src_face_embedding_list=get_face_embedding(src_image_list,mtcnn,iresnet,10)
+        face_consistency_list=[]
+        face_target_similarity_list=[]
+        for i,face_embedding in enumerate(image_face_embedding_list):
+            for j,secondary_embedding in enumerate(image_face_embedding_list[i+1:]):
+                face_consistency_list.append(cos_sim(secondary_embedding,face_embedding))
+            for k,src_face_embedding in enumerate(src_face_embedding_list):
+                face_target_similarity_list.append(cos_sim(face_embedding,src_face_embedding))
+        metric_dict[FACE_CONSISTENCY]=np.mean(face_consistency_list)
+        metric_dict[FACE_TARGET_SIMILARITY]=np.mean(face_target_similarity_list)
+
     for metric in METRIC_LIST:
         if metric not in metric_dict:
             metric_dict[metric]=0.0
