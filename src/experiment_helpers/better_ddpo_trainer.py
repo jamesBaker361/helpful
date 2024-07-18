a3c06113ade4fb4580f35e4f2539d44b13f81bc2
James Baker <jlbaker361@gmail.com>
2024-07-18 01:16:55 -0400

    subject key

--- src/experiment_helpers/better_ddpo_trainer.py
+++ src/experiment_helpers/better_ddpo_trainer.py
@@ -99,6 +99,7 @@ class BetterDDPOTrainer(BaseTrainer):
         prompt_function: Callable[[], Tuple[str, Any]],
         sd_pipeline: BetterDefaultDDPOStableDiffusionPipeline,
         image_samples_hook: Optional[Callable[[Any, Any, Any], Any]] = None,
+        subject_key: Optional[str]=""
     ):
         if image_samples_hook is None:
             warn("No image_samples_hook provided; no images will be logged")
@@ -107,6 +108,7 @@ class BetterDDPOTrainer(BaseTrainer):
         self.reward_fn = reward_function
         self.config = config
         self.image_samples_callback = image_samples_hook
+        self.subject_key=subject_key
 
         accelerator_project_config = ProjectConfiguration(**self.config.project_kwargs)
 
@@ -297,6 +299,12 @@ class BetterDDPOTrainer(BaseTrainer):
                 "reward_std": rewards.std(),
             }
         )
+        if self.subject_key!="":
+            self.accelerator.log({
+                f"{self.subject_key}_reward": rewards,
+                f"{self.subject_key}_reward_mean": rewards.mean(),
+                f"{self.subject_key}_reward_std": rewards.std(),
+            })
 
         if self.config.per_prompt_stat_tracking:
             # gather the prompts across processes
